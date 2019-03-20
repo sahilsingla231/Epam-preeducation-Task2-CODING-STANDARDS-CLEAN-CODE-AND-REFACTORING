e9dca5e46bb24a4cba6c5f53b98701f3287e5605
sahilsingla231 <sahilsingla231231@gmail.com>
2019-03-20 15:21:41 +0530

    Upadated

--- SimpleAndCompoundInterest/src/SimpleAndCompoundInterestTest.java
+++ SimpleAndCompoundInterest/src/SimpleAndCompoundInterestTest.java
@@ -7,8 +7,8 @@ class SimpleAndCompoundInterestTest {
 	@Test
 	void testSimpleIntrest() {
 		SimpleAndCompoundInterest simpleIntrest;
-		simpleIntrest = new SimpleAndCompoundInterest(20202, 2.5f, 3);
-		assertEquals(1515.1500244140625, simpleIntrest.calcSimpleIntrest());
+		simpleIntrest = new SimpleAndCompoundInterest(1512, 3f, 2);
+		assertEquals(90.72000122070312, simpleIntrest.calcSimpleIntrest());
 		simpleIntrest = new SimpleAndCompoundInterest(20202, 2.5f, 5);
 		assertEquals(2525.25, simpleIntrest.calcSimpleIntrest());
 	}
@@ -16,8 +16,8 @@ class SimpleAndCompoundInterestTest {
 	@Test
 	void testCompoundIntrest() {
 		SimpleAndCompoundInterest compoundIntrest;
-		compoundIntrest = new SimpleAndCompoundInterest(20202, 2.5f, 3);
-		assertEquals(21755.342888139312, compoundIntrest.calcCompoundIntrest());
+		compoundIntrest = new SimpleAndCompoundInterest(1512, 3f, 2);
+		assertEquals(1604.0807108871472, compoundIntrest.calcCompoundIntrest());
 		compoundIntrest = new SimpleAndCompoundInterest(20202, 2.5f, 5);
 		assertEquals(22856.7060585414, compoundIntrest.calcCompoundIntrest());
 	}
